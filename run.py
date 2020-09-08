c9cb5edb1a7fe8082bf995df155fdabc14d213b9
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-08 22:32:10 +0300

    Introduce plotting.

--- run.py
+++ run.py
@@ -3,11 +3,19 @@ import os
 
 print("First of all, we need to extract features from wav-files\n"
       "We will use librosa - a Python lib for audio processing")
+
 import librosa
 
-# from all the good files
+print("For plotting, we need - matplotlib\n - a wrapper from librosa")
+import matplotlib.pyplot as plt
+import librosa.display
+
+DISPLAY_PLOT_WAVE = True
+DISPLAY_PLOT_STFT = True
+
+# for all the good files
 for i in [j for j in os.listdir() if j.endswith('wav')]:
-    print("loading the wav using the default sample rate = 22050 %s" % i)
+    print("loading the wav using default sampling rate = 22050 %s" % i)
 
     wav_i, sr = librosa.load(i)
     assert sr == 22050
@@ -18,6 +26,15 @@ for i in [j for j in os.listdir() if j.endswith('wav')]:
           "--- using the deafult sample rate length of the windowed\n"
           "--- signal (TODO change later) and other defaults")
 
+    if DISPLAY_PLOT_WAVE == True:
+        librosa.display.waveplot(wav_i)
+        plt.show()
+
     stft = librosa.stft(wav_i)
+
+    if DISPLAY_PLOT_STFT == True:
+        librosa.display.specshow(stft)
+        plt.show()
+
 
     break
