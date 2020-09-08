aa7469741f931fc02c4f8f5dfd33ea52a8bafe55
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-08 21:55:15 +0300

    Improve formatting. Try calculating STFT.

--- run.py
+++ run.py
@@ -1,15 +1,23 @@
 #!/usr/bin/env python
 import os
 
-print("First of all, we need to extract features from wav-files")
-print("We will use librosa - a Python lib for audio processing - for that")
+print("First of all, we need to extract features from wav-files\n"
+      "We will use librosa - a Python lib for audio processing")
 import librosa
 
 # from all the good files
 for i in [j for j in os.listdir() if j.endswith('wav')]:
-    print("extracting features from %s" % i)
+    print("loading the wav using the default sample rate = 22050 %s" % i)
+
+    wav_i, sr = librosa.load(i)
+    assert sr == 22050
+
     print("- using librosa Core API functions just to check what they provide")
     print("-- spectral representations")
-    print("--- short-time fourier transform")
+    print("--- short-time fourier transform a.k.a. `librosa.stft`\n"
+          "--- using the deafult sample rate length of the windowed\n"
+          "--- signal (TODO change later) and other defaults")
+
+    stft = librosa.stft(wav_i)
 
     break
