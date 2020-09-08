823a8c64e8a2b172d6eca2cdbfec36ebc847eedc
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-08 21:37:36 +0300

    Use librosa Core API to see how we can visualize a sound.

--- run.py
+++ run.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python
 import os
 
-## extract features
+print("First of all, we need to extract features from wav-files")
+print("We will use librosa - a Python lib for audio processing - for that")
+import librosa
 
 # from all the good files
 for i in [j for j in os.listdir() if j.endswith('wav')]:
-    print(i)
+    print("extracting features from %s" % i)
+    print("- using librosa Core API functions just to check what they provide")
+    print("-- spectral representations")
+    print("--- short-time fourier transform")
+
+    break
