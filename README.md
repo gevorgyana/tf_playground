329066d33cb4dbffabe42d425b7f1f925ec043f8
Artyom Gevorgyan <artemhevorhian@gmail.com>
2020-09-09 15:33:53 +0300

    Prepared a mini-version of the dataset. See variable `bound`.

--- README.md
+++ README.md
@@ -5,6 +5,17 @@ Todo:
 prepare the whole dataset into a stadard form, insipired by this video:
 https://www.youtube.com/watch?v=szyGiObZymo
 
+--- [quote]
+
+The dataset consists of 50 WAV files sampled at 16KHz for 50 different
+classes.
+
+To each one of the classes, corresponds 40 audio sample of 5 seconds each.
+All of these audio files have been concatenated by class in order to have
+50 wave files of 3 min. 20sec.
+
+---
+
 [Download this](https://www.kaggle.com/mmoreaux/environmental-sound-classification-50)
 And place in the same directory as where this projects is located.
 
--- run.py
+++ run.py
@@ -11,86 +11,97 @@ import matplotlib.pyplot as plt
 import librosa.display
 import numpy as np
 
-DISPLAY_PLOT_WAVE = False
-DISPLAY_PLOT_STFT = False
-DISPLAY_WITH_MANUALLY_CHANGED_SCALE = False
+print
+(
+    "The initial dataset contains .wavs, we need Mel Spectrograms\n"
+    "Therefore we need to build them first.\n"
+    "But we need to get markers from the .csv file that comes with the "
+    "dataset"
+)
 
-# should always be True for the scirpt to work
-POWER_TO_DB = True
+# Returns the dictionary mapping each number into its corresponding MFCC
+def prepare_mfccs():
+    id2mfcc = {}
+    for dirpath, dirnames, filenames in os.walk("kaggle_ds"):
+        if dirpath == '.':
+            continue
 
-def demo_librosa():
-    # for all the good files
-    for i in [j for j in os.listdir() if j.endswith('wav')]:
-        print("loading the wav using default sampling rate = 22050 %s"
-              % i)
+        counter = 0
+        bound = 3
 
-        wav_i, sr = librosa.load(i)
-        assert sr == 22050
+        for f in filenames:
+            if counter == bound:
+                break
+            if f.endswith('wav'):
+                counter += 1
+                waveform, sr = librosa.load("./{}/{}".format(dirpath, f))
+                '''
+                id2mfcc[f] = librosa.power_to_db (
+                    librosa.feature.melspectrogram(waveform, sr = sr),
+                    ref = np.max
+                )
+                '''
 
-        print("- using librosa Core API functions to check what they provide")
-        print("-- spectral representations")
-        print("--- short-time fourier transform a.k.a. `librosa.stft`\n"
-              "--- using the deafult sample rate length of the windowed\n"
-              "--- signal (TODO change later) and other defaults")
+                # See the docs for this function, it is customizable, but
+                # I am using defaults. There is a difference between
+                # mfcc and melspectrogram, but he chooses to use
+                # the former ¯\_(ツ)_/¯
+                # [#] https://www.youtube.com/watch?v=szyGiObZymo
 
-        if DISPLAY_PLOT_WAVE == True:
-            librosa.display.waveplot(wav_i)
-            plt.show()
+                id2mfcc[f] = librosa.feature.mfcc(waveform, sr = sr)
 
-        # hop length = 512 is a smart default that gives the most precision
-        # https://dsp.stackexchange.com/questions/248/how-do-i-optimize-the-window-lengths-in-stft, also may need the default value too
-        stft = librosa.stft(wav_i, hop_length = 512)
-
-        if DISPLAY_PLOT_STFT == True:
-            librosa.display.specshow(stft)
-            plt.show()
-
-        print("- What happens if we take the absolute values?")
-        stft = np.abs(stft)
-        if DISPLAY_PLOT_STFT == True:
-            librosa.display.specshow(stft)
-            plt.title("Now showing the absolute values")
-            plt.show()
-        print("- stft is now np.abs(stft)")
-
-        print("- Manually change the y-scale to Decebel scale")
-        stft_log = librosa.amplitude_to_db(stft, ref = np.max)
-
-        if DISPLAY_WITH_MANUALLY_CHANGED_SCALE == True:
-            librosa.display.specshow(stft_log, sr=sr,
-                                     x_axis = 'time', y_axis = 'log')
-            plt.show()
-
-        # Seems like the previous image is an MFCC, and there are many
-        # ways to create one in librosa, but I will use the funcition that
-        # is specifically tailored for that
-
-        print("Calculate the MFCC")
-        # - As suggested by docs, TODO tweak `fmax` - it may be necessary to
-        # remove all of the zero-ed out freqeuncies at the top of MFCC.
-        # - Use this method, not custom-made conversions!!!!!
-
-        S = librosa.feature.melspectrogram(wav_i, sr)
-        # The example in the docs runs this too, but IDK if that is needed.
-
-        # Disapling this will lead to a bad output - so we need this thing
-        # to get a prper MFCC
-        if POWER_TO_DB == True:
-            S = librosa.power_to_db(S, ref = np.max)
-            librosa.display.specshow(S, x_axis = 'time', y_axis = 'mel',
-                                     sr = sr, fmax = 8000)
-            plt.show()
-        print("Successful")
-
-# demo_librosa()
-
-'''
-print("\n --- \n")
-print("Preparing the dataset : building a native Python dictionary")
+    return id2mfcc
 
 def prepare_data():
-    for i, (dirpath, dirnames, filenames) in enumerate(os.walk(".")):
-        print("{} {} {} {}".format(i, dirpath, dirnames, filenames))
+
+    prepared_data = {
+        "mfccs": prepare_mfccs(),
+        "names": [],
+        "labels": [],
+    }
+
+    for i, (dirpath, dirnames, filenames) in enumerate (
+            os.walk("kaggle_ds")
+    ):
+        if dirpath == '.':
+            continue
+
+        for f in filenames:
+            if f.endswith('wav'):
+                prepared_data["names"].append (
+                    f
+                )
+
+    prepared_data["names"] = prepared_data["names"][:3]
+
+    print("alive!")
+
+    # fill in the labels from the .csv file inside of `kaggle_ds`
+    for dirpath, _, files in os.walk("kaggle_ds"):
+        for f in files:
+            if not f.endswith('csv'):
+                continue
+
+            csv_name = "./{}/{}".format(dirpath, f)
+            print(csv_name)
+            with open(csv_name) as csv:
+                for i in csv:
+                    columns = i.split(',')
+                    name = columns[0]
+
+                    # NOTODO quadratic algorithm is okay, Python is slow
+                    # anyway :S
+
+                    for name_picked in prepared_data['names']:
+                        if name_picked == name:
+                            # need its label only? TODO think about it
+                            label = columns[3]
+
+                            prepared_data['labels'].append(
+                                label
+                            )
+
+    print(prepared_data)
+    return prepared_data
 
 prepare_data()
-'''
